11302ba75e86426a6494a5618c583e4a0e00649f
Alinaufalova <65111595+Alinaufalova@users.noreply.github.com>
2020-09-26 21:58:07 +0500

    Add files via upload

---
+++ assignment2/src/com/company/EmployeePr.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class EmployeePr {
+    void info();
+}
---
+++ assignment2/src/com/company/Person.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Person {
+    public Person(String name, String lastName) {
+    }
+}
